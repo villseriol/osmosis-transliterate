69b0c98756086f2e7c61f84ac16c2bee9a8237ca
EY4180 <44942476+EY4180@users.noreply.github.com>
2026-08-17 20:25:31 +0800

    Added Explicit Mapping For Letterlike

--- src/main/java/org/villseriol/osmosis/transliterate/v0_6/unicode/mapping/characterset/LetterlikeMapper.java
+++ src/main/java/org/villseriol/osmosis/transliterate/v0_6/unicode/mapping/characterset/LetterlikeMapper.java
@@ -4,8 +4,6 @@ package org.villseriol.osmosis.transliterate.v0_6.unicode.mapping.characterset;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.villseriol.osmosis.transliterate.v0_6.unicode.mapping.nonspecific.RangeToWhitespaceTransform;
-import org.villseriol.osmosis.transliterate.v0_6.unicode.types.UnicodeRange;
 import org.villseriol.osmosis.transliterate.v0_6.unicode.types.Unimap;
 
 import com.ibm.icu.text.ReplaceableString;
@@ -14,7 +12,6 @@ import com.ibm.icu.text.Transliterator;
 
 // https://www.unicodepedia.com/groups/letterlike-symbols/
 public class LetterlikeMapper implements Unimap {
-    private static final Unimap FALLBACK = new RangeToWhitespaceTransform(UnicodeRange.LETTERLIKE_SYMBOLS);
     private static final Transliterator TRANSLITERATOR;
 
     static {
@@ -30,6 +27,7 @@ public class LetterlikeMapper implements Unimap {
         rules.add("℄ > 'CL';"); // 2104 CENTRE LINE SYMBOL (arbitrary)
         rules.add("℅ > 'c/o';"); // 2105 CARE OF
         rules.add("℆ > 'c/u';"); // 2106 CADA UNA
+        rules.add("ℇ > ' ';"); // 2107 EULER CONSTANT
         rules.add("℈ > 's';"); // 2108 SCRUPLE (apothecary weight, arbitrary)
         rules.add("℉ > '°F';"); // 2109 DEGREE FAHRENHEIT
         rules.add("ℊ > 'g';"); // 210A SCRIPT SMALL G
@@ -60,7 +58,11 @@ public class LetterlikeMapper implements Unimap {
         rules.add("™ > '(TM)';"); // 2122 TRADE MARK SIGN
         rules.add("℣ > 'V';"); // 2123 VERSICLE (liturgical, arbitrary)
         rules.add("ℤ > 'Z';"); // 2124 DOUBLE-STRUCK CAPITAL Z
+        rules.add("℥ > ' ';"); // 2125 OUNCE SIGN
+        rules.add("Ω > ' ';"); // 2126 OHM SIGN
+        rules.add("℧ > ' ';"); // 2127 INVERTED OHM SIGN
         rules.add("ℨ > 'Z';"); // 2128 BLACK-LETTER CAPITAL Z (dup)
+        rules.add("℩ > ' ';"); // 2129 TURNED GREEK SMALL LETTER IOTA
         rules.add("K > 'K';"); // 212A KELVIN SIGN
         rules.add("Å > 'A';"); // 212B ANGSTROM SIGN
         rules.add("ℬ > 'B';"); // 212C SCRIPT CAPITAL B
@@ -69,19 +71,36 @@ public class LetterlikeMapper implements Unimap {
         rules.add("ℯ > 'e';"); // 212F SCRIPT SMALL E (dup)
         rules.add("ℰ > 'E';"); // 2130 SCRIPT CAPITAL E
         rules.add("ℱ > 'F';"); // 2131 SCRIPT CAPITAL F
+        rules.add("Ⅎ > ' ';"); // 2132 TURNED CAPITAL F
         rules.add("ℳ > 'M';"); // 2133 SCRIPT CAPITAL M
         rules.add("ℴ > 'o';"); // 2134 SCRIPT SMALL O
         rules.add("ℵ > 'N';"); // 2135 ALEF SYMBOL
+        rules.add("ℶ > ' ';"); // 2136 BET SYMBOL
+        rules.add("ℷ > ' ';"); // 2137 GIMEL SYMBOL
+        rules.add("ℸ > ' ';"); // 2138 DALET SYMBOL
         rules.add("ℹ > 'i';"); // 2139 INFORMATION SOURCE
+        rules.add("℺ > ' ';"); // 213A ROTATED CAPITAL Q
         rules.add("℻ > 'FAX';"); // 213B FACSIMILE SIGN
+        rules.add("ℼ > ' ';"); // 213C DOUBLE-STRUCK SMALL PI
         rules.add("ℽ > 'y';"); // 213D DOUBLE-STRUCK SMALL GAMMA
+        rules.add("ℾ > ' ';"); // 213E DOUBLE-STRUCK CAPITAL GAMMA
+        rules.add("ℿ > ' ';"); // 213F DOUBLE-STRUCK CAPITAL PI
+        rules.add("⅀ > ' ';"); // 2140 DOUBLE-STRUCK N-ARY SUMMATION
+        rules.add("⅁ > ' ';"); // 2141 TURNED SANS-SERIF CAPITAL G
+        rules.add("⅂ > ' ';"); // 2142 TURNED SANS-SERIF CAPITAL L
+        rules.add("⅃ > ' ';"); // 2143 REVERSED SANS-SERIF CAPITAL L
+        rules.add("⅄ > ' ';"); // 2144 TURNED SANS-SERIF CAPITAL Y
         rules.add("ⅅ > 'D';"); // 2145 DOUBLE-STRUCK ITALIC CAPITAL D
         rules.add("ⅆ > 'd';"); // 2146 DOUBLE-STRUCK ITALIC SMALL D
         rules.add("ⅇ > 'e';"); // 2147 DOUBLE-STRUCK ITALIC SMALL E (dup)
         rules.add("ⅈ > 'i';"); // 2148 DOUBLE-STRUCK ITALIC SMALL I
         rules.add("ⅉ > 'j';"); // 2149 DOUBLE-STRUCK ITALIC SMALL J
+        rules.add("⅊ > ' ';"); // 214A PROPERTY LINE
+        rules.add("⅋ > ' ';"); // 214B TURNED AMPERSAND
         rules.add("⅌ > '/';"); // 214C PER SIGN (arbitrary)
         rules.add("⅍ > 'A/S';"); // 214D AKTIESELSKAB
+        rules.add("ⅎ > ' ';"); // 214E TURNED SMALL F
+        rules.add("⅏ > ' ';"); // 214F SYMBOL FOR SAMARITAN SOURCE
 
         TRANSLITERATOR = Transliterator.createFromRules("Letterlike-BasicLatin", String.join("\n", rules),
                 Transliterator.FORWARD);
@@ -89,13 +108,12 @@ public class LetterlikeMapper implements Unimap {
 
     @Override
     public String action(String input) {
-        return FALLBACK.action(TRANSLITERATOR.transliterate(input));
+        return TRANSLITERATOR.transliterate(input);
     }
 
 
     @Override
     public void action(StringBuffer input) {
         TRANSLITERATOR.transliterate(new ReplaceableString(input));
-        FALLBACK.action(input);
     }
 }
