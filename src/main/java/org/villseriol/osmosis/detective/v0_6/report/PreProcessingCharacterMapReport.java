9a236ebd0fc628baeae16a08cb2ccd275f6ffcee
EY4180 <44942476+EY4180@users.noreply.github.com>
2026-08-19 21:15:43 +0800

    Added Unicode Names to PreProcessing Report

--- src/main/java/org/villseriol/osmosis/detective/v0_6/report/PreProcessingCharacterMapReport.java
+++ src/main/java/org/villseriol/osmosis/detective/v0_6/report/PreProcessingCharacterMapReport.java
@@ -43,19 +43,27 @@ public class PreProcessingCharacterMapReport extends DetReport {
         int fromHeaderCol = cursor.getCol();
         row.createCell(fromHeaderCol).setCellValue("From");
         sheet.setColumnWidth(fromHeaderCol, 32);
+        int fromNameHeaderCol = cursor.nextCol();
+        row.createCell(fromNameHeaderCol).setCellValue("From Name");
+        sheet.setColumnWidth(fromNameHeaderCol, 128);
         int toHeaderCol = cursor.nextCol();
         row.createCell(toHeaderCol).setCellValue("To");
         sheet.setColumnWidth(toHeaderCol, 32);
-        int occurencesHeaderCol = cursor.nextCol();
-        row.createCell(occurencesHeaderCol).setCellValue("Occurences");
-        sheet.setColumnWidth(occurencesHeaderCol, 32);
+        int toNameHeaderCol = cursor.nextCol();
+        row.createCell(toNameHeaderCol).setCellValue("To Name");
+        sheet.setColumnWidth(toNameHeaderCol, 128);
+        int occurrencesHeaderCol = cursor.nextCol();
+        row.createCell(occurrencesHeaderCol).setCellValue("Occurrences");
+        sheet.setColumnWidth(occurrencesHeaderCol, 32);
         int reservedHeaderCol = cursor.nextCol();
         row.createCell(reservedHeaderCol).setCellValue("Reserved");
         sheet.setColumnWidth(reservedHeaderCol, 32);
 
         row.getCell(fromHeaderCol).setCellStyle(headerStyle);
+        row.getCell(fromNameHeaderCol).setCellStyle(headerStyle);
         row.getCell(toHeaderCol).setCellStyle(headerStyle);
-        row.getCell(occurencesHeaderCol).setCellStyle(headerStyle);
+        row.getCell(toNameHeaderCol).setCellStyle(headerStyle);
+        row.getCell(occurrencesHeaderCol).setCellStyle(headerStyle);
         row.getCell(reservedHeaderCol).setCellStyle(headerStyle);
 
         cursor.resetCol();
@@ -69,8 +77,14 @@ public class PreProcessingCharacterMapReport extends DetReport {
         int fromCol = cursor.getCol();
         row.createCell(fromCol).setCellValue(record.getFrom().toString());
 
+        int fromNameCol = cursor.nextCol();
+        row.createCell(fromNameCol).setCellValue(record.getFromName());
+
         int toCol = cursor.nextCol();
         row.createCell(toCol).setCellValue(record.getTo().toString());
+
+        int toNameCol = cursor.nextCol();
+        row.createCell(toNameCol).setCellValue(record.getToName());
 
         cursor.nextCol();
 
