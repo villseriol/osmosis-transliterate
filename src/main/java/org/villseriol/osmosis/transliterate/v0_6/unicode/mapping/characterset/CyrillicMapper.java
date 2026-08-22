3d3c11986728728faa482c43507216b45e02d251
EY4180 <44942476+EY4180@users.noreply.github.com>
2026-08-22 08:55:11 +0800

    Cyrillic Now Uses Annotation Format

--- src/main/java/org/villseriol/osmosis/transliterate/v0_6/unicode/mapping/characterset/CyrillicMapper.java
+++ src/main/java/org/villseriol/osmosis/transliterate/v0_6/unicode/mapping/characterset/CyrillicMapper.java
@@ -1,16 +1,28 @@
 // This software is released into the Public Domain.  See copying.txt for details.
 package org.villseriol.osmosis.transliterate.v0_6.unicode.mapping.characterset;
 
+import java.util.Arrays;
+
+import org.villseriol.osmosis.transliterate.v0_6.unicode.Icu4jUtils;
+import org.villseriol.osmosis.transliterate.v0_6.unicode.UnicodeRange;
 import org.villseriol.osmosis.transliterate.v0_6.unicode.Unimap;
+import org.villseriol.osmosis.transliterate.v0_6.unicode.reflection.UnicodeRanges;
 
 import com.ibm.icu.text.ReplaceableString;
 import com.ibm.icu.text.Transliterator;
 
 
-// https://www.unicodepedia.com/groups/cyrillic/
+@UnicodeRanges(UnicodeRange.CYRILLIC)
 public class CyrillicMapper implements Unimap {
-    private static final Transliterator TRANSLITERATOR = Transliterator
-            .getInstance("[[:Cyrillic:]] Cyrillic-Latin; Latin-ASCII");
+    private static final Transliterator TRANSLITERATOR;
+
+    static {
+        UnicodeRange[] ranges = Icu4jUtils.getAnnotatedUnicodeRanges(CyrillicMapper.class);
+        UnicodeRange[] additional = Arrays.copyOfRange(ranges, 1, ranges.length);
+        String filter = Icu4jUtils.toIcuRange(ranges[0], additional);
+
+        TRANSLITERATOR = Transliterator.getInstance(filter + " Cyrillic-Latin; Latin-ASCII");
+    }
 
     @Override
     public String action(String input) {
