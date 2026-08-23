ea2f99b8f651b532835819938b2be532fe99c91e
EY4180 <44942476+EY4180@users.noreply.github.com>
2026-08-23 08:21:21 +0800

    Created a Mock Pipeline Runner Mirroring Production

--- src/test/java/org/villseriol/osmosis/transliterate/v0_6/TransliteratePipelineRunnerTest.java
+++ src/test/java/org/villseriol/osmosis/transliterate/v0_6/TransliteratePipelineRunnerTest.java
@@ -6,35 +6,15 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 import org.openstreetmap.osmosis.testutil.AbstractDataTest;
-import org.villseriol.osmosis.transliterate.v0_6.config.loader.XmlLoader;
-import org.villseriol.osmosis.transliterate.v0_6.config.model.NormalizeConfiguration;
+import org.villseriol.osmosis.transliterate.v0_6.mock.MockTransliteratePipelineRunner;
 
 
 public class TransliteratePipelineRunnerTest extends AbstractDataTest {
-    private final TransliteratePipelineRunner preRunner = new TransliteratePipelineRunner();
-    private final TransliteratePipelineRunner nameRunner = new TransliteratePipelineRunner();
-    private final TransliteratePipelineRunner postRunner = new TransliteratePipelineRunner();
-    private final XmlLoader<NormalizeConfiguration> loader = XmlLoader.getInstance(NormalizeConfiguration.class);
+    private MockTransliteratePipelineRunner runner;
 
     @Before
     public void setUp() {
-        preRunner.init(loadConfig("v0_6/pre-processing-config.xml"));
-        nameRunner.init(loadConfig("v0_6/standard-name-config.xml"));
-        postRunner.init(loadConfig("v0_6/post-processing-config.xml"));
-    }
-
-
-    private NormalizeConfiguration loadConfig(String path) {
-        return loader.load(dataUtils.createDataFile(path));
-    }
-
-
-    private String run(String tag, String value) {
-        value = preRunner.run(tag, value);
-        value = nameRunner.run(tag, value);
-        value = postRunner.run(tag, value);
-
-        return value;
+        runner = new MockTransliteratePipelineRunner(dataUtils);
     }
 
 
@@ -45,37 +25,37 @@ public class TransliteratePipelineRunnerTest extends AbstractDataTest {
     public void testArrows() {
         // 1770382887
         assertEquals("Mt. Misen·Komaga-bayashi<-->Ohmoto-Park",
-                run("name:en", "Mt. Misen・Komaga-bayashi←→Ohmoto-Park"));
+                runner.run("Mt. Misen・Komaga-bayashi←→Ohmoto-Park"));
 
         // NB: not correct translation. Consider an skk dictionary for mountains
-        assertEquals("ya yama·koma hayashi<-->oomoto kouen", run("name", "弥山・駒ヶ林←→大元公園"));
+        assertEquals("ya yama·koma hayashi<-->oomoto kouen", runner.run("弥山・駒ヶ林←→大元公園"));
 
         // 12755242465
         // NB: guideposts to use the address dictionary
         // NB: arrow transform now only supports left/right arrows; diagonal
         // arrows are dropped to whitespace and trimmed away
-        assertEquals("5-cho old path", run("name:en", "5-chō old path↖"));
-        assertEquals("go chou ko michi", run("name", "五丁古道↖"));
-        assertEquals("itchou", run("name", "↙一丁"));
+        assertEquals("5-cho old path", runner.run("5-chō old path↖"));
+        assertEquals("go chou ko michi", runner.run("五丁古道↖"));
+        assertEquals("itchou", runner.run("↙一丁"));
 
         // 5649766468
         // NB: maybe all nodes related to transportation should use the address
-        assertEquals("shikoku kisen feri^ uno<=>miya ura (choku shima)", run("name", "四国汽船 フェリー　宇野⇔宮浦(直島)"));
+        assertEquals("shikoku kisen feri^ uno<=>miya ura (choku shima)", runner.run("四国汽船 フェリー　宇野⇔宮浦(直島)"));
 
         // 9196762039
         // NB: once again, applying the address-based dictionary shows better
         // translation results
         assertEquals("ehara eigyousho [han 01·han 02<magomeeki mae basu tei=>2 ban noriba basu tei he>]",
-                run("name", "荏原営業所【反０１・反０２＜馬込駅前バス停⇒２番乗り場バス停へ＞】"));
+                runner.run("荏原営業所【反０１・反０２＜馬込駅前バス停⇒２番乗り場バス停へ＞】"));
 
         assertEquals("furenzu (FRIENDS)|sayamashi no mouhatsu soudan dekiru biyou shitsu",
-                run("name", "フレンズ（FRIENDS）│狭山市の毛髪相談できる美容室"));
-        assertEquals("kyouto yodo izakaya meibutsu karaage", run("name", "京都🏮淀🍶居酒屋🍗名物からあげ"));
+                runner.run("フレンズ（FRIENDS）│狭山市の毛髪相談できる美容室"));
+        assertEquals("kyouto yodo izakaya meibutsu karaage", runner.run("京都🏮淀🍶居酒屋🍗名物からあげ"));
         assertEquals("oka kuchi 1,260m / kaneoka kouen 530m / shimin no ki yanagi",
-                run("name", "岡口 1,260m / 金岡公園530m▶ / 市民の木 ヤナギ"));
-        assertEquals("moto otafuku->tsunku no mise->", run("name", "元 おたふく→つんく♂の店→"));
+                runner.run("岡口 1,260m / 金岡公園530m▶ / 市民の木 ヤナギ"));
+        assertEquals("moto otafuku->tsunku no mise->", runner.run("元 おたふく→つんく♂の店→"));
         assertEquals(
                 "sendaizaka [toukyuu] no [1 ban noriba basu tei] (2) sendaizaka [keikyuu] no [B ban noriba basu tei]",
-                run("name", "仙台坂【★東急】の【１番乗り場バス停】 ②仙台坂【☆京急】の【Ｂ番乗り場バス停】"));
+                runner.run("仙台坂【★東急】の【１番乗り場バス停】 ②仙台坂【☆京急】の【Ｂ番乗り場バス停】"));
     }
 }
